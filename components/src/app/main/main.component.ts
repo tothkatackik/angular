527c80e4ab6e5667289bffd32dddb25549da0d5e
Tóth Kata <toth.kata@ckik.hu>
2026-09-14 08:06:33 +0200

    asd

---
+++ components/src/app/main/main.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  imports: [],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.scss',
+})
+export class MainComponent {}
